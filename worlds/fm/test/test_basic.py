6d313e59687a880f1a7cbc5ba121141c9e7bc5e2
sg4e <46875746+sg4e@users.noreply.github.com>
2024-02-02 01:54:54 +0000

    Add tests for more options

--- worlds/fm/test/test_basic.py
+++ worlds/fm/test/test_basic.py
@@ -5,13 +5,18 @@ from ..utils import flatten
 from ..duelists import get_duelist_defeat_location_name
 
 
+base_options = {
+    "duelist_progression": "campaign",
+    "final6_sequence": "vanilla",
+    "final6_progression": "fixed",
+    "atec_logic": "off",
+    "invisible_wire_logic": False,
+    "drop_rate_logic": 4,
+}
+
+
 class TestBasic(FMTestBase):
-    options = {
-        "duelist_progression": "campaign",
-        "final6_sequence": "vanilla",
-        "final6_progression": "fixed",
-        "atec_logic": "off"
-    }
+    options = base_options
 
     def test_cannot_access_final_boss_at_start(self) -> None:
         self.assertFalse(self.can_reach_location("Nitemare defeated"))
@@ -23,15 +28,6 @@ class TestBasic(FMTestBase):
         self.assert_cannot_reach_location("Widespread Ruin in library")
         self.collect(prog_items[8])
         self.assert_can_reach_location("Widespread Ruin in library")
-
-
-base_options = {
-    "final6_sequence": "vanilla",
-    "final6_progression": "fixed",
-    "atec_logic": "off",
-    "invisible_wire_logic": "Off",
-    "drop_rate_logic": 4,
-}
 
 
 # I see no way how to parameterize this at the class level
@@ -49,8 +45,8 @@ def impl_test_duelist_progression_unlocks(test: FMTestBase, unlock_order):
 
 class TestThematicProgression(FMTestBase):
     options = {
-        "duelist_progression": "thematic",
-        **base_options
+        **base_options,
+        "duelist_progression": "thematic"
     }
 
     def test_thematic_progression(self) -> None:
@@ -59,8 +55,8 @@ class TestThematicProgression(FMTestBase):
 
 class TestCampaignProgression(FMTestBase):
     options = {
-        "duelist_progression": "campaign",
-        **base_options
+        **base_options,
+        "duelist_progression": "campaign"
     }
 
     def test_campaign_progression(self) -> None:
@@ -69,9 +65,41 @@ class TestCampaignProgression(FMTestBase):
 
 class TestSingularProgression(FMTestBase):
     options = {
-        "duelist_progression": "singular",
-        **base_options
+        **base_options,
+        "duelist_progression": "singular"
     }
 
     def test_singular_progression(self) -> None:
         impl_test_duelist_progression_unlocks(self, duelist_progression_map[DuelistProgression.option_singular])
+
+
+class TestShuffledFinal6Progression(FMTestBase):
+    options = {
+        **base_options,
+        "final6_progression": "shuffled"
+    }
+
+
+class TestShuffleFinal5Order(FMTestBase):
+    options = {
+        **base_options,
+        "final6_sequence": "first_5_shuffled"
+    }
+
+
+class TestShuffleFinal6Order(FMTestBase):
+    options = {
+        **base_options,
+        "final6_sequence": "all_6_shuffled"
+    }
+
+
+class TestMostRandom(FMTestBase):
+    options = {
+        "duelist_progression": "singular",
+        "final6_sequence": "all_6_shuffled",
+        "final6_progression": "shuffled",
+        "atec_logic": "all",
+        "invisible_wire_logic": False,
+        "drop_rate_logic": 0,
+    }
